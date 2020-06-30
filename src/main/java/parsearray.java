cbc41c38b250ca2d70f0338d8293846818d79235
Dimitri <semionov@users.noreply.github.com>
2020-06-30 19:59:06 +0200

    Initial commit

---
+++ src/main/java/parsearray.java
@@ -0,0 +1,14 @@
+/*import org.json.JSONArray;
+
+public class parsearray {
+
+    JSONArray temp = cuestionario.getJSONArray("name");
+    int length = temp.length();
+if (length > 0) {
+        String [] recipients = new String [length];
+        for (int i = 0; i < length; i++) {
+            recipients[i] = temp.getString(i);
+        }
+    }
+}
+*/
